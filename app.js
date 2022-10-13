bc03e20457ebfb15d74c3561719d57730dbf6b8f
eyetech2dev <eyetech2dev@gmail.com>
2022-10-13 17:22:41 +1100

    add express-session cookies

--- app.js
+++ app.js
@@ -34,6 +34,20 @@ app.use(methodOverride("_method"));
 // serve static assets
 app.use(express.static(path.join(__dirname, "public")));
 
+// Express session
+const sessionConfig = {
+  secret: "thisshouldbeabettersecret!",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
+};
+app.use(session(sessionConfig));
+
+// Express router
 app.use("/campgrounds", campgrounds);
 app.use("/campgrounds/:id/reviews", reviews);
 
